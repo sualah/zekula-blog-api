486d43b0e3d1ddb1403c26e0cfedb7689ae9bec9
sualah <sabdullai@acsint.io>
2023-11-18 17:31:40 +0000

    chore(app):added spring boot project dependencies

---
+++ src/main/java/com/zekula/blog/ZekulaBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.zekula.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZekulaBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZekulaBlogRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zekula/blog/ZekulaBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekula.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZekulaBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
